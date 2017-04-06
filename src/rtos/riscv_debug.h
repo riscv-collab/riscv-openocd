fab7311f18c49be82e8e1681c8966e576525d3ed
Palmer Dabbelt <palmer@dabbelt.com>
2017-04-06 15:24:17 -0700

    archive branch

--- src/rtos/riscv_debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef RTOS__RISCV_H
-#define RTOS__RISCV_H
-
-struct riscv_rtos {
-	/* The index into the thread list used to handle */
-	int qs_thread_info_offset;
-};
-
-#endif
--- tools/checkpatch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-
-since=${1:-HEAD^}
-git format-patch -M --stdout $since | tools/scripts/checkpatch.pl - --no-tree
