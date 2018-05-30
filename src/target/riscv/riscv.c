2ec501a8b3718d2e1f487ee3b93e2413233c33ea
Palmer Dabbelt <palmer@dabbelt.com>
2018-05-30 06:00:14 -0700

    Revert "double result"
    This reverts commit 1b227f1f49b1a8f53d8577a547368207f898c026.

--- src/target/riscv/riscv.c
+++ src/target/riscv/riscv.c
@@ -692,7 +692,7 @@ static int riscv_examine(struct target *target)
 	if (result != ERROR_OK)
 		return result;
 
-	result = tt->examine(target);
+	int result = tt->examine(target);
 	if (result != ERROR_OK)
 		return result;
 
