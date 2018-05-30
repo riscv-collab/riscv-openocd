e373719ac115ebb6d93fa343ad64c7bcda58afcf
Palmer Dabbelt <palmer@dabbelt.com>
2018-05-30 00:05:27 -0700

    Don't halt whenever GDB attaches

--- src/target/startup.tcl
+++ src/target/startup.tcl
@@ -203,7 +203,6 @@ proc init_target_events {} {
 	foreach t $targets {
 		set_default_target_event $t gdb-flash-erase-start "reset init"
 		set_default_target_event $t gdb-flash-write-end "reset halt"
-		set_default_target_event $t gdb-attach "halt"
 	}
 }
 
