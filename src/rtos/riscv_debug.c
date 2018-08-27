openocd-org/openocd

commit 8ede238449661581b31e09b7d328ae4d9b51406d

Handle the qC packet (#292)
* Handle the qC packet

GDB sends the qC packet to find out which thread OpenOCD is on

* Correct format string for qC packet

src/rtos/riscv_debug.c
@@ -124,6 +124,13 @@ static int riscv_gdb_thread_packet(struct connection *connection, const char *pa
 			return ERROR_OK;
 		}
 
+		if (strncmp(packet, "qC", 2) == 0) {
+			char rep_str[32];
+			snprintf(rep_str, 32, "QC%" PRIx64, rtos->current_threadid);
+			gdb_put_packet(connection, rep_str, strlen(rep_str));
+			return ERROR_OK;
+		}
+
 		return GDB_THREAD_PACKET_NOT_CONSUMED;
 
 	case 'Q':

riscv-collab/riscv-openocd

commit afa0b434817e05d4dd54ba025513da898df54186

Match on qC, but not qCRC.
Now we pass MulticoreRtosSwitchActiveHartTest again.
It was broken by #292.

Change-Id: I61f0ae41efda09fd48f732b2122fed2400a43d29

src/rtos/riscv_debug.c
@@ -124,7 +124,7 @@ static int riscv_gdb_thread_packet(struct connection *connection, const char *pa
 			return ERROR_OK;
 		}
 
-		if (strncmp(packet, "qC", 2) == 0) {
+		if (strcmp(packet, "qC") == 0) {
 			char rep_str[32];
 			snprintf(rep_str, 32, "QC%" PRIx64, rtos->current_threadid);
 			gdb_put_packet(connection, rep_str, strlen(rep_str));