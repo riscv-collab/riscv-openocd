bdb12359223cfebd680f15d366c1bfa8c2ec3acd
Samuel Obuch <sobuch@codasip.com>
2020-09-15 10:21:18 +0200

    Improve riscv expose_[csrs|custom] commands
    * Add option to specify custom name for registers.
    * Allow to call commands multiple times without loss of previous data.
    * Make sure the commands can only be used in the config phase (before "init").
    * Validity checks and warnings.
    * Change commands to be per target.
    * Fix memory leaks.
    * Also fix unrelated memory leaks to keep valgrind happy.
    
    Signed-off-by: Samuel Obuch <sobuch@codasip.com>

--- src/target/riscv/riscv-013.c
+++ src/target/riscv/riscv-013.c
@@ -286,6 +286,7 @@ dm013_info_t *get_dm(struct target *target)
 	}
 	target_entry = calloc(1, sizeof(*target_entry));
 	if (!target_entry) {
+		free(info->dm);
 		info->dm = NULL;
 		return NULL;
 	}
@@ -1537,10 +1538,17 @@ int wait_for_authbusy(struct target *target, uint32_t *dmstatus)
 static void deinit_target(struct target *target)
 {
 	LOG_DEBUG("riscv_deinit_target()");
-	riscv_info_t *info = (riscv_info_t *) target->arch_info;
-	free(info->version_specific);
+	RISCV_INFO(r);
+	RISCV013_INFO(info);
+
+	target_list_t *entry, *tmp;
+	list_for_each_entry_safe(entry, tmp, &info->dm->target_list, list)
+		free(entry);
+
+	free(info->dm);
+	free(r->version_specific);
 	/* TODO: free register arch_info */
-	info->version_specific = NULL;
+	r->version_specific = NULL;
 }
 
 static int set_haltgroup(struct target *target, bool *supported)
