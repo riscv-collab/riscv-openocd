9bf770b04f529ae60f6735702c6ab7b8bc1148da
Jan Matyas <jmatyas@codasip.com>
2021-03-15 21:24:56 +0100

    Changed toolchain to: xPack GNU RISC-V Embedded GCC v10.1.0-1.1
    Change-Id: Ic285cd0147fb9285adb5edb20f27b0a3e06ff82a
    Signed-off-by: Jan Matyas <jmatyas@codasip.com>

--- tools/riscv-openocd-ci/download_toolchain.sh
+++ tools/riscv-openocd-ci/download_toolchain.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-TOOLCHAIN_URL="https://buildbot.embecosm.com/job/riscv32-gcc-ubuntu1804"
-TOOLCHAIN_URL+="/25/artifact/riscv32-embecosm-gcc-ubuntu1804-20201108.tar.gz"
+# Toolchain builds provided by Embecosm (buildbot.embecosm.com)
+# TOOLCHAIN_URL="https://buildbot.embecosm.com/job/riscv32-gcc-ubuntu1804"
+# TOOLCHAIN_URL+="/25/artifact/riscv32-embecosm-gcc-ubuntu1804-20201108.tar.gz"
+# TOOLCHAIN_PREFIX=riscv32-unknown-elf-
+
+# Toolchain builds from "The xPack Project"
+# (https://xpack.github.io/riscv-none-embed-gcc/)
+TOOLCHAIN_URL="https://github.com/xpack-dev-tools/riscv-none-embed-gcc-xpack/"
+TOOLCHAIN_URL+="releases/download/v10.1.0-1.1/"
+TOOLCHAIN_URL+="xpack-riscv-none-embed-gcc-10.1.0-1.1-linux-x64.tar.gz"
+TOOLCHAIN_PREFIX=riscv-none-embed-
+
 ARCHIVE_NAME=${TOOLCHAIN_URL##*/}
 DOWNLOAD_DIR=`pwd`/tools/riscv-openocd-ci/work
 INSTALL_DIR=`pwd`/tools/riscv-openocd-ci/work/install
@@ -23,9 +33,16 @@ mkdir -p "$INSTALL_DIR"
 cd "$INSTALL_DIR"
 tar xvf "$DOWNLOAD_DIR/$ARCHIVE_NAME" --strip-components=1
 
-# Make symlinks: riscv64-* --> riscv32-*
 cd "$INSTALL_DIR/bin"
-find . -name 'riscv32-*' | while read F; do ln -s $F $(echo $F | sed -e 's/riscv32/riscv64/'); done
 
-# Check that the compiler runs
+# Make symlinks so that the tools are accessible as riscv64-unknown-elf-*
+if [ "$TOOLCHAIN_PREFIX" != "riscv64-unknown-elf-" ]; then
+    find . -name "$TOOLCHAIN_PREFIX*" | while read F; do
+        ln -s $F $(echo $F | sed -e "s/$TOOLCHAIN_PREFIX/riscv64-unknown-elf-/");
+    done
+fi
+
+# Check that the compiler and debugger run
 ./riscv64-unknown-elf-gcc --version
+./riscv64-unknown-elf-gdb --version
+
