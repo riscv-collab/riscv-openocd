142f948be8a8d049ce7c96073f5bc4df3e8bf536
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2022-11-11 14:46:32 +0300

    [config][syntacore] added <srst delay>,<srst pulse_width>, and <ftdi_tdo_sample_edge> parameters #19066

--- testsuite/ocd.syntacore/not_testable.tcl
+++ testsuite/ocd.syntacore/not_testable.tcl
@@ -6,10 +6,10 @@ sc_target_config gdb_report_register_access_error 0
 sc_target_config expose_csrs 928=some_register,1952-1953,2048
 
 sc_target_config adapter_speed 1
-# sc_target_config adapter_srst_pulse_width 777
-# sc_target_config adapter_srst_delay 42
+sc_target_config adapter_srst_pulse_width 777
+sc_target_config adapter_srst_delay 42
 
-# sc_target_config ftdi_tdo_sample_edge falling
+sc_target_config ftdi_tdo_sample_edge falling
 
 init
 
