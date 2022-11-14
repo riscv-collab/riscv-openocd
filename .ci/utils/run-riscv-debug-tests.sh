6732cb767b80c3afa6aa54b195a2e083af03cf3d
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2022-11-14 14:52:02 +0300

    [ci] adjust run-riscv-debug-tests to address post-commit review comments

--- .ci/utils/run-riscv-debug-tests.sh
+++ .ci/utils/run-riscv-debug-tests.sh
@@ -9,12 +9,13 @@ RESULT="SUCCESS"
 mkdir -p ${LOGS}/${TGT}
 
 ${ROOT}/gdbserver.py ${ROOT}/targets/RISC-V/${TGT}.py  \
+  --isolate               \
   --logs "${LOGS}/${TGT}" \
-  --print-failures             \
-  --gcc "${GCC}"               \
-  --gdb "${GDB}"               \
-  --sim_cmd "${SIM}"           \
-  --server_cmd "${OCD}" | tee "${LOGS}/${TGT}.log"
+  --print-failures        \
+  --gcc "${GCC}"          \
+  --gdb "${GDB}"          \
+  --sim_cmd "${SIM}"      \
+  --server_cmd "${OCD}" 2>&1 | tee "${LOGS}/${TGT}.log"
 
 TEST_STATUS=${PIPESTATUS[0]}
 if [[ $TEST_STATUS -ne 0 ]]; then
