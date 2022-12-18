4a54444d7d804ead06db491564a623801d1f6f9f
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2022-12-18 12:53:42 +0300

    [ci] try to download dependency from artifactory cache #19902

--- .ci/utils/fetch_dependencies.py
+++ .ci/utils/fetch_dependencies.py
@@ -10,8 +10,6 @@ input_file = sys.argv[1]
 
 ARTIFACTORY_URL = "http://artifactory.dev.syntacore.com:8082/artifactory"
 ARTIFACTORY_DIR = 'openocd_build_dependencies'
-os.putenv('ARTIFACTORY_DIR', ARTIFACTORY_DIR)
-os.putenv('SKIP_ARCHIVATION', 'true')
 
 class CommandFailedError(Exception):
   pass
@@ -20,32 +18,41 @@ with open(input_file, "r") as stream:
   deps = yaml.safe_load(stream)
 
 def run_command(cmd):
-  print(cmd)
+  print(cmd, file=sys.stderr, flush=True)
   if os.system(cmd) != 0:
     raise CommandFailedError("failed to succesfully execute command")
 
-for item, desc in deps.items():
-  URL = desc['URL']
+def download_file(item, desc):
+
+  O_URL = desc['URL']
   MD5 = desc['MD5']
   HOW = desc['HOW']
   DST = desc['DST']
+
   if DST == "-":
     DST = item
 
-  print("")
-  if os.getenv('DOWNLOAD_DEPENDENCIES_FROM_ARTIFACTORY', 'false') == 'true':
-    URL = f"{ARTIFACTORY_URL}/{ARTIFACTORY_DIR}/{DST}"
-  cmd = f"{HOW} -L {URL} -o {DST}"
-  md5_expected = f"echo {MD5} {DST} >{DST}.md5.expected"
-  md5_check = f"md5sum --check {DST}.md5.expected"
-  if os.path.isfile(f"{DST}"):
-    print(f"{DST} exists, download SKIPPED")
-  else:
-    run_command(cmd)
-  run_command(md5_expected)
-  run_command(md5_check)
+  A_URL = f"{ARTIFACTORY_URL}/{ARTIFACTORY_DIR}/{DST}"
+  O_URL = desc['URL']
 
-  if os.getenv('ARTIFACTORY_UPLOAD', 'false') == 'true':
-    utils_dir = os.path.dirname(__file__)
-    upload_command = f"{utils_dir}/upload_artifact.sh {DST}"
-    run_command(upload_command)
+  print(f"trying to fetch {DST}", file=sys.stderr, flush=True)
+  for url in [A_URL, O_URL]:
+    try:
+      cmd = f"{HOW} -L {url} -o {DST}"
+      md5_expected = f"echo {MD5} {DST} >{DST}.md5.expected"
+      md5_check = f"md5sum --check {DST}.md5.expected"
+      if os.path.isfile(f"{DST}"):
+        print(f"{DST} exists, download SKIPPED")
+      else:
+        run_command(cmd)
+      run_command(md5_expected)
+      run_command(md5_check)
+      return
+    except CommandFailedError:
+      print(f"could not get file from {url}")
+  raise CommandFailedError(f"could not download {DST}")
+
+for item, desc in deps.items():
+  print("", file=sys.stderr, flush=True)
+
+  download_file(item, desc)
