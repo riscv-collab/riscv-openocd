995e728c45bdbe073f1c17a9afb10e05a7051420
Evgeniy Naydanov <evgeniy.naydanov@syntacore.com>
2023-05-23 09:55:19 +0300

    [ci] riscv-tests/debug: pass -d3 flag to openocd

--- .makepy/support/dependencies_support/run-riscv-debug-tests.sh
+++ .makepy/support/dependencies_support/run-riscv-debug-tests.sh
@@ -11,11 +11,10 @@ mkdir -p ${LOGS}/${TGT}
 ${ROOT}/gdbserver.py ${ROOT}/targets/RISC-V/${TGT}.py  \
   --isolate               \
   --logs "${LOGS}/${TGT}" \
-  --print-failures        \
   --gcc "${GCC}"          \
   --gdb "${GDB}"          \
   --sim_cmd "${SIM}"      \
-  --server_cmd "${OCD}" 2>&1 | tee "${LOGS}/${TGT}.log"
+  --server_cmd "${OCD} -d3" 2>&1 | tee "${LOGS}/${TGT}.log"
 
 TEST_STATUS=${PIPESTATUS[0]}
 if [[ $TEST_STATUS -ne 0 ]]; then
