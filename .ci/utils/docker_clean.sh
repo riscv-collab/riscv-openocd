8af3cbdcff6aaa7f1183caecddea88cc85ebbac7
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2023-05-24 14:21:10 +0300

    [ci] more aggressive cleanup for jenkins pipelines #29085

--- .ci/utils/docker_clean.sh
+++ .ci/utils/docker_clean.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker container ls --quiet --all --filter "name=^makepy-cicd-sc-openocd_sources.*" | while read image_id ; do
+docker container ls --quiet --all --filter "name=^makepy-.*-openocd_sources.*" | while read image_id ; do
   echo === $image_id ===
   docker stop $image_id
   docker rm $image_id
