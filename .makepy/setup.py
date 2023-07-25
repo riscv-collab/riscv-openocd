116501f3c060f9ac18431be9e526f1bc7e3f945b
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2023-07-26 00:41:16 +0300

    [build] removed diff-index calls #31343

--- .makepy/setup.py
+++ .makepy/setup.py
@@ -197,13 +197,7 @@ class _ConfigCommand(_Command):
     def command(self, args: _Namespace) -> None:
         _shutil.rmtree(_repo_path / "build-aux", ignore_errors=True)
         # NOTE: we expect OpenOCD submodules to be initialized at this point
-        _run_shell(
-            ["git", "diff-index", "--compact-summary", "HEAD"], cwd=_repo_path
-        )
         _run_shell(["./bootstrap", "nosubmodule"], cwd=_repo_path)
-        _run_shell(
-            ["git", "diff-index", "--compact-summary", "HEAD"], cwd=_repo_path
-        )
         cmd = [
             "cmake",
             "-S",
@@ -214,9 +208,6 @@ class _ConfigCommand(_Command):
             (args.build_path / "conan_toolchain.cmake").absolute(),
         ]
         _run_shell(cmd)
-        _run_shell(
-            ["git", "diff-index", "--compact-summary", "HEAD"], cwd=_repo_path
-        )
 
 
 class _BuildCommand(_Command):
@@ -248,13 +239,7 @@ class _BuildCommand(_Command):
         ]
         if args.logging_level == "DEBUG":
             cmd.append("--verbose")
-        _run_shell(
-            ["git", "diff-index", "--compact-summary", "HEAD"], cwd=_repo_path
-        )
         _run_shell(cmd)
-        _run_shell(
-            ["git", "diff-index", "--compact-summary", "HEAD"], cwd=_repo_path
-        )
 
 
 def _sources(kind: str | list[str], path: _Path = _repo_path) -> list[_Path]:
