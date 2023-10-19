b16dc1f8ddbafadd15b41485aa124a09ad471500
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2023-10-19 20:08:32 +0300

    [syntacore] get rid of most adapter-specific parameters from syntacore_target

--- testsuite/ocd.syntacore/not_testable.tcl
+++ testsuite/ocd.syntacore/not_testable.tcl
@@ -6,13 +6,6 @@ sc_target_config gdb_report_register_access_error 0
 sc_target_config expose_csrs 928=some_register,1952-1953,2048
 
 sc_target_config adapter_speed 1
-sc_target_config adapter_srst_pulse_width 777
-sc_target_config adapter_srst_delay 42
-
-# NOTE: if the selected driver is not ftdi, setting this option is an error
-if { [adapter driver] == "ftdi" } {
- sc_target_config ftdi_tdo_sample_edge falling
-}
 
 init
 
