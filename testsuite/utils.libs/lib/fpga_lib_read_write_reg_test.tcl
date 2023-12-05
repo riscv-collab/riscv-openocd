9293c7896da6e75a8ef98e2fcc23a0694c7e6c47
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2023-12-05 23:52:29 +0300

    [tests] extended fpga_lib_read_write_reg_test to actually test write function YCAT-37576

--- testsuite/utils.libs/lib/fpga_lib_read_write_reg_test.tcl
+++ testsuite/utils.libs/lib/fpga_lib_read_write_reg_test.tcl
@@ -2,6 +2,15 @@ proc reg_value {raw_value} {
   return [string trim [lindex [split $raw_value :] 1]]
 }
 
+proc test_write_and_readback { reg_name value } {
+  sc_fpga_write_reg $reg_name $value
+  set readback [sc_fpga_read_reg $reg_name]
+  if {$readback != $value} {
+    error "could not read expected value $value from $reg_name, got $readback"
+  }
+  echo ""
+}
+
 proc run_test { elf entry_point xlen} {
   set entry_point [expr $entry_point]
   sc_fpga_run_elf_smp $elf $entry_point
@@ -35,6 +44,22 @@ proc run_test { elf entry_point xlen} {
   if {$fp_from_lib ne $expected_fp} {
     error "expected_fp is $expected_fp, while lib returned $fp_from_lib"
   }
+  set UNSUPPORTED_REGISTER blablablableblue
+  if {![catch { sc_fpga_read_reg $UNSUPPORTED_REGISTER }]} {
+    error "unexpected success while reading $UNSUPPORTED_REGISTER"
+  }
+  if {![catch { sc_fpga_write_reg $UNSUPPORTED_REGISTER 100 }] } {
+    error "unexpected success while writing $UNSUPPORTED_REGISTER"
+  }
+
+  set FP_VAL100 100
+  set FP_VAL200 200
+  set FP_VALX300 0x300
+
+  test_write_and_readback fp $FP_VAL100
+  test_write_and_readback fp $FP_VAL200
+  test_write_and_readback fp $FP_VALX300
+  test_write_and_readback fp $expected_fp
 }
 
 proc run_read_write_reg_test { elf entry_point xlen} {
