d3cc03fd680079d8c28f79cc69f928fdc13673f7
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2024-04-06 09:18:39 +0300

    support for self-contained debug adapter configs YCAT-39720

--- testsuite/boards/support/stand_db/stand_db.tcl
+++ testsuite/boards/support/stand_db/stand_db.tcl
@@ -1,48 +1,50 @@
 proc standInfoGet { parameter } {
   global ADAPTER_INFO
-  if { ![info exists ADAPTER_INFO] } {
-    global OPENOCD_DEBUG_ADAPTER_CONFIG
-    global OPENOCD_DEBUG_ADAPTER_SERIAL
-    global OPENOCD_DEBUG_ADAPTER_SPEED
-    global OPENOCD_ROOT
+  if { [info exists ADAPTER_INFO] } {
+    return $ADAPTER_INFO($parameter)
+  }
 
-    if { ${OPENOCD_DEBUG_ADAPTER_CONFIG} eq "" } {
-      puts "Debug Adapter Configuration is not defined for the target board"
-      puts "Please, check OPENOCD_DEBUG_ADAPTER_CONFIG variable"
-      error "OPENOCD_DEBUG_ADAPTER_CONFIG is empty"
-    }
+  global OPENOCD_DEBUG_ADAPTER_CONFIG
+  global OPENOCD_DEBUG_ADAPTER_SERIAL
+  global OPENOCD_DEBUG_ADAPTER_SPEED
+  global OPENOCD_ROOT
 
-    if { ${OPENOCD_DEBUG_ADAPTER_SERIAL} eq "" } {
-      puts "Debug Adapter Serial is not defined for the target board"
-      puts "Please, check OPENOCD_DEBUG_ADAPTER_SERIAL variable"
-      error "OPENOCD_DEBUG_ADAPTER_SERIAL is empty"
-    }
+  if { ${OPENOCD_DEBUG_ADAPTER_CONFIG} eq "" } {
+    puts "Debug Adapter Configuration is not defined for the target board"
+    puts "Please, check OPENOCD_DEBUG_ADAPTER_CONFIG variable"
+    error "OPENOCD_DEBUG_ADAPTER_CONFIG is empty"
+  }
 
-    if { ${OPENOCD_DEBUG_ADAPTER_SPEED} eq "" } {
-      puts "Debug Adapter Speed is not defined for the target board"
-      puts "Please, check OPENOCD_DEBUG_ADAPTER_SPEED variable"
-      error "OPENOCD_DEBUG_ADAPTER_SPEED is empty"
-    }
-
+  if {[info exists OPENOCD_DEBUG_ADAPTER_SPEED]} {
     set ADAPTER_INFO(default_speed) ${OPENOCD_DEBUG_ADAPTER_SPEED}
-    set ADAPTER_INFO(adapter_id) ${OPENOCD_DEBUG_ADAPTER_SERIAL}
-    if { [file pathtype ${OPENOCD_DEBUG_ADAPTER_CONFIG}] eq "absolute" } {
-      set ADAPTER_INFO(adapter_config) "${OPENOCD_DEBUG_ADAPTER_CONFIG}"
-      set ADAPTER_INFO(adapter_config_relative) ""
-      set ROOT_TEST ${OPENOCD_ROOT}
-      if { [string match */ $ROOT_TEST] == 0 } {
-        set ROOT_TEST "${ROOT_TEST}/"
-      }
-      if { [string first ${ROOT_TEST} ${OPENOCD_DEBUG_ADAPTER_CONFIG}] == 0} {
-        set cut_from [string length ${ROOT_TEST}]
-        set ADAPTER_INFO(adapter_config_relative) \
-          [string range ${OPENOCD_DEBUG_ADAPTER_CONFIG} $cut_from end]
-      }
-    } else {
-      set ADAPTER_INFO(adapter_config) \
-        [file join ${OPENOCD_ROOT} ${OPENOCD_DEBUG_ADAPTER_CONFIG}]
-      set ADAPTER_INFO(adapter_config_relative) ${OPENOCD_DEBUG_ADAPTER_CONFIG}
+  } else {
+    verbose "WARNING: OPENOCD_DEBUG_ADAPTER_SPEED is NOT specified" 1
+    set ADAPTER_INFO(default_speed) ""
+  }
+
+  if {[info exists OPENOCD_DEBUG_ADAPTER_SERIAL] } {
+    set ADAPTER_INFO(adapter_serial) ${OPENOCD_DEBUG_ADAPTER_SERIAL}
+  } else {
+    verbose "WARNING: OPENOCD_DEBUG_ADAPTER_SERIAL is NOT specified" 1
+    set ADAPTER_INFO(adapter_serial) ""
+  }
+
+  if { [file pathtype ${OPENOCD_DEBUG_ADAPTER_CONFIG}] eq "absolute" } {
+    set ADAPTER_INFO(adapter_config) "${OPENOCD_DEBUG_ADAPTER_CONFIG}"
+    set ADAPTER_INFO(adapter_config_relative) ""
+    set ROOT_TEST ${OPENOCD_ROOT}
+    if { [string match */ $ROOT_TEST] == 0 } {
+      set ROOT_TEST "${ROOT_TEST}/"
     }
+    if { [string first ${ROOT_TEST} ${OPENOCD_DEBUG_ADAPTER_CONFIG}] == 0} {
+      set cut_from [string length ${ROOT_TEST}]
+      set ADAPTER_INFO(adapter_config_relative) \
+        [string range ${OPENOCD_DEBUG_ADAPTER_CONFIG} $cut_from end]
+    }
+  } else {
+    set ADAPTER_INFO(adapter_config) \
+      [file join ${OPENOCD_ROOT} ${OPENOCD_DEBUG_ADAPTER_CONFIG}]
+    set ADAPTER_INFO(adapter_config_relative) ${OPENOCD_DEBUG_ADAPTER_CONFIG}
   }
   return $ADAPTER_INFO($parameter)
 }
@@ -60,10 +62,21 @@ proc standInfoGetInterfaceFlags { } {
     ]
   }
 
-  return [list \
-    -f "[standInfoGet adapter_config]" \
-    -c "transport select jtag" \
-    -c "adapter speed [standInfoGet default_speed]" \
-    -c "adapter serial [standInfoGet adapter_id]" \
-  ]
+  set interface_flags [list]
+  lappend interface_flags -f
+  lappend interface_flags [standInfoGet adapter_config]
+
+  set adapter_speed_override [standInfoGet default_speed]
+  if { ${adapter_speed_override} != "" } {
+    lappend interface_flags -c
+    lappend interface_flags "adapter speed ${adapter_speed_override}"
+  }
+
+  set adapter_serial_override [standInfoGet adapter_serial]
+  if { ${adapter_serial_override} != "" } {
+    lappend interface_flags -c
+    lappend interface_flags "adapter serial ${adapter_serial_override}"
+  }
+
+  return $interface_flags
 }
