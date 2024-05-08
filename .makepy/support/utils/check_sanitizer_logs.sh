09bab80eeb202bbb7cf0f5c5dbce0c9fbe052c08
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2024-05-08 07:39:45 +0300

    [ci] fix and improve ubsan/valgrind logs analysis YCAT-39692,YCAT-39542

--- .makepy/support/utils/check_sanitizer_logs.sh
+++ .makepy/support/utils/check_sanitizer_logs.sh
@@ -4,22 +4,41 @@ set -o errexit
 set -o nounset
 
 TEST_DIRECTORY="$1"
-UBSAN_COUNT=$(find "$TEST_DIRECTORY" -name "*.log" -type f -exec  grep --text -R "runtime error" {} \; | grep jim.c -v | wc -l)
+TD_LEN=$(($(echo -n "${TEST_DIRECTORY}/" | wc -c)+1))
 
+UBSAN_ERRS_FILE="${TEST_DIRECTORY}/ubsan.errs"
+VALGRIND_ERRS_FILE="${TEST_DIRECTORY}/valgrind.errs"
+
+find "${TEST_DIRECTORY}" -name "*.log" -type f -exec  grep --text -H -R "runtime error" {} \; \
+  | grep jim.c -v \
+  | sort \
+  | cut -c${TD_LEN}- >"${UBSAN_ERRS_FILE}"
+UBSAN_COUNT=$(cat "${UBSAN_ERRS_FILE}" | wc -l)
+
+find "${TEST_DIRECTORY}" -name "*.log" -type f -exec  grep --text -H -R "== ERROR SUMMARY:" {} \; \
+  | grep " 0 errors from" -v \
+  | sort \
+  | cut -c${TD_LEN}- >"${VALGRIND_ERRS_FILE}"
+VALGRIND_COUNT=$(cat "${VALGRIND_ERRS_FILE}" | wc -l)
+
+
+RETURN_CODE=0
 if [ "$UBSAN_COUNT" -ne 0 ] ;
 then
+  RETURN_CODE=1
   echo "ERROR: $UBSAN_COUNT unexpected sanitizer errors detected!"
-  exit 1
+  cat "${UBSAN_ERRS_FILE}"
+else
+  echo "SUCCESS: no unexpected sanitizer errors detected"
 fi
-
-echo "SUCCESS: no unexpected sanitizer errors detected"
-
-VALGRIND_COUNT=$(find "$TEST_DIRECTORY" -name "*.log" -type f -exec  grep --text -R "== ERROR SUMMARY:" {} \; | grep "0 errors from" -v | wc -l)
 
 if [ "$VALGRIND_COUNT" -ne 0 ] ;
 then
+  RETURN_CODE=1
   echo "ERROR: $VALGRIND_COUNT unexpected valgrind errors detected!"
-  exit 1
+  cat "${VALGRIND_ERRS_FILE}"
+else
+  echo "SUCCESS: no unexpected valgrind errors detected"
 fi
 
-echo "SUCCESS: no unexpected valgrind errors detected"
+exit ${RETURN_CODE}
