openocd-org/openocd

commit e4ee891759b08d3edb258b34f00b4ae8e3298d06

improve buf_cmp and buf_cmp_mask helpers
Rewrite buf_cmp to use memcpy for bulk of comparison.  Add static
helper to perform comparison of trailing byte, which uses another
static helper to perform a maksed comparison.  The masked comparison
helper is used by the buf_cmp_mask to simplify its loop.
Improve types to use void *, unsigned, and return bool.

src/helper/binarybuffer.c
@@ -65,51 +65,52 @@ uint8_t* buf_cpy(const uint8_t *from, uint8_t *to, int size)
 	return to;
 }
 
-int buf_cmp(const uint8_t *buf1, const uint8_t *buf2, int size)
+static bool buf_cmp_masked(uint8_t a, uint8_t b, uint8_t m)
 {
-	if (!buf1 || !buf2)
-		return 1;
-
-	for (unsigned i = 0, num_bytes = CEIL(size, 8); i < num_bytes; i++)
-	{
-		/* last byte */
-		/* mask out bits that don't really belong to the buffer if size isn't a multiple of 8 bits */
-		if ((size % 8) && (i == num_bytes -1))
-		{
-			if ((buf1[i] & ((1 << (size % 8)) - 1)) != (buf2[i] & ((1 << (size % 8)) - 1)))
-				return 1;
-		}
-		else
-		{
-			if (buf1[i] != buf2[i])
-				return 1;
-		}
-	}
-
-	return 0;
+	return (a & m) != (b & m);
+}
+static bool buf_cmp_trailing(uint8_t a, uint8_t b, uint8_t m, unsigned trailing)
+{
+	uint8_t mask = (1 << trailing) - 1;
+	return buf_cmp_masked(a, b, mask & m);
 }
 
-int buf_cmp_mask(const uint8_t *buf1, const uint8_t *buf2, const uint8_t *mask, int size)
+bool buf_cmp(const void *_buf1, const void *_buf2, unsigned size)
 {
-	for (unsigned i = 0, num_bytes = CEIL(size, 8); i < num_bytes; i++)
-	{
-		/* last byte */
-		/* mask out bits that don't really belong to the buffer if size isn't a multiple of 8 bits */
-		if ((size % 8) && (i == num_bytes -1))
-		{
-			if ((buf1[i] & ((1 << (size % 8)) - 1) & mask[i]) !=
-				(buf2[i] & ((1 << (size % 8)) - 1) & mask[i]))
-				return 1;
-		}
-		else
-		{
-			if ((buf1[i] & mask[i]) != (buf2[i] & mask[i]))
-				return 1;
-		}
-	}
+	if (!_buf1 || !_buf2)
+		return _buf1 != _buf2;
 
-	return 0;
+	unsigned last = size / 8;
+	if (memcmp(_buf1, _buf2, last) != 0)
+		return false;
+
+	unsigned trailing = size % 8;
+	if (!trailing)
+		return false;
+
+	const uint8_t *buf1 = _buf1, *buf2 = _buf2;
+	return buf_cmp_trailing(buf1[last], buf2[last], 0xff, trailing);
 }
+
+bool buf_cmp_mask(const void *_buf1, const void *_buf2,
+		const void *_mask, unsigned size)
+{
+	if (!_buf1 || !_buf2)
+		return _buf1 != _buf2 || _buf1 != _mask;
+
+	const uint8_t *buf1 = _buf1, *buf2 = _buf2, *mask = _mask;
+	unsigned last = size / 8;
+	for (unsigned i = 0; i < last; i++)
+	{
+		if (buf_cmp_masked(buf1[i], buf2[i], mask[i]))
+			return true;
+	}
+	unsigned trailing = size % 8;
+	if (!trailing)
+		return false;
+	return buf_cmp_trailing(buf1[last], buf2[last], mask[last], trailing);
+}
+
 
 uint8_t* buf_set_ones(uint8_t *buf, int count)
 {

src/helper/binarybuffer.h
@@ -70,9 +70,9 @@ static inline uint32_t buf_get_u32(const uint8_t* buffer,
 /// flip_u32 inverts the bit order inside a 32-bit word (31..0 -> 0..31)
 uint32_t flip_u32(uint32_t value, unsigned int num);
 
-int buf_cmp(const uint8_t *buf1, const uint8_t *buf2, int size);
-int buf_cmp_mask(const uint8_t *buf1, const uint8_t *buf2,
-		const uint8_t *mask, int size);
+bool buf_cmp(const void *buf1, const void *buf2, unsigned size);
+bool buf_cmp_mask(const void *buf1, const void *buf2,
+		const void *mask, unsigned size);
 uint8_t* buf_cpy(const uint8_t *from, uint8_t *to, int size);
 
 uint8_t* buf_set_ones(uint8_t *buf, int count);

riscv-collab/riscv-openocd

commit 0188d803e3fa0189b8b5de21aa2f8ed80a1facbe

[BACKPORT][CHERRY-PICK] binarybuffer: Fix inverted return value in buf_cmp
This is the fast path for when there is a mismatch in the leading whole
bytes, which means we should return true to indicate not equal like all
the other cases here and in the surrounding functions. Otherwise we'll
incorrectly report _buf1 == _buf2 if and only if there are mismatches in
the leading whole bytes.

This was introduced during the refactor and optimisation referenced
below.

The only in-tree caller of this is jtag_check_value_inner, which will
just fail to catch some errors. However, downstream in riscv-openocd it
gets used in the riscv target to determine whether an IR scan is needed
to select the debug module, and with an IRLEN >= 8 this breaks resetting
if the encoding for the DMI isn't all-ones in its leading whole bytes
(to match BYPASS), since it will believe they are the same and not do an
IR scan, failing (with "At least one TAP shouldn't be in BYPASS mode")
in the subsequent DR scan due to the TAP still being recorded as having
bypass set (and really having an instruction of either BYPASS or
IDCODE).

Fixes: e4ee891759b0 ("improve buf_cmp and buf_cmp_mask helpers")
Change-Id: Ic4f7ed094429abc4c06a775eb847a8b3ddf2e2d6
Signed-off-by: Jessica Clarke <jrtc27@jrtc27.com>

src/helper/binarybuffer.c
@@ -74,7 +74,7 @@ bool buf_cmp(const void *_buf1, const void *_buf2, unsigned size)
 
 	unsigned last = size / 8;
 	if (memcmp(_buf1, _buf2, last) != 0)
-		return false;
+		return true;
 
 	unsigned trailing = size % 8;
 	if (!trailing)