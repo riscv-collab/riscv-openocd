8dce03eb333bf170a5fb71206016256b83f84901
Parshintsev Anatoly <anatoly.parshintsev@syntacore.com>
2024-11-08 12:34:04 +0300

    [syntacore][tests] extend tests targeting expose_csr to include hexadecimal register addresses YCAT-41321

--- testsuite/ocd.syntacore/universal_config_tests/not_testable.tcl
+++ testsuite/ocd.syntacore/universal_config_tests/not_testable.tcl
@@ -3,7 +3,7 @@
 sc_target_config gdb_report_data_abort 0
 sc_target_config gdb_report_register_access_error 0
 
-sc_target_config expose_csrs 928=some_register,1952-1953,2048
+sc_target_config expose_csrs 928=some_register,1952-1953,2048,0x801,0x802=some_register_hex,0x803-0x804
 
 sc_target_config adapter_speed 1
 
