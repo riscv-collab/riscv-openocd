openocd-org/openocd

commit 908ee4dc9641bd3df2eb00264575501867da539d

build: remove clang unused variable assignment warnings
Change-Id: Ibe5254704d6cd879a318a82c4f50d9da3c14276c
Signed-off-by: Spencer Oliver <spen@spen-soft.co.uk>
Reviewed-on: http://openocd.zylin.com/600
Tested-by: jenkins
Reviewed-by: Freddie Chopin <freddie.chopin@gmail.com>

src/rtos/linux.c
@@ -302,7 +302,7 @@ static int linux_os_thread_reg_list(struct rtos *rtos,
 				hex_string += sprintf(hex_string, "%02x", 0);
 
 			uint32_t cpsr = 0x00000000;
-			hex_string = reg_converter(hex_string, &cpsr, 4);
+			reg_converter(hex_string, &cpsr, 4);
 		}
 	}
 	return ERROR_OK;
@@ -769,12 +769,12 @@ int linux_get_tasks(struct target *target, int context)
 	struct threads *last = NULL;
 	t->base_addr = linux_os->init_task_addr;
 	/* retrieve the thread id , currently running in the different smp core */
-	retval = get_current(target, 1);
+	get_current(target, 1);
 
 	while (((t->base_addr != linux_os->init_task_addr) &&
 		(t->base_addr != 0)) || (loop == 0)) {
 		loop++;
-		retval = fill_task(target, t);
+		fill_task(target, t);
 		retval = get_name(target, t);
 
 		if (loop > MAX_THREADS) {
@@ -1214,7 +1214,7 @@ int linux_thread_extra_info(struct target *target,
 			tmp_str_ptr +=
 				sprintf(tmp_str_ptr, "%d", (int)temp->pid);
 			tmp_str_ptr += sprintf(tmp_str_ptr, "%s", " | ");
-			tmp_str_ptr += sprintf(tmp_str_ptr, "%s", name);
+			sprintf(tmp_str_ptr, "%s", name);
 			sprintf(tmp_str_ptr, "%s", temp->name);
 			char *hex_str =
 				(char *)calloc(1, strlen(tmp_str) * 2 + 1);

src/flash/nor/at91sam4.c
@@ -849,9 +849,9 @@ static int FLASHD_GetLockBits(struct sam4_bank_private *pPrivate, uint32_t *v)
 	LOG_DEBUG("Here");
 	r = EFC_PerformCommand(pPrivate, AT91C_EFC_FCMD_GLB, 0, NULL);
 	if (r == ERROR_OK)	{
-		r = EFC_GetResult(pPrivate, v);
-		r = EFC_GetResult(pPrivate, v);
-		r = EFC_GetResult(pPrivate, v);
+		EFC_GetResult(pPrivate, v);
+		EFC_GetResult(pPrivate, v);
+		EFC_GetResult(pPrivate, v);
 		r = EFC_GetResult(pPrivate, v);
 	}
 	LOG_DEBUG("End: %d", r);

src/flash/nor/fm3.c
@@ -579,7 +579,6 @@ static int fm3_probe(struct flash_bank *bank)
 
 	bank->sectors = malloc(sizeof(struct flash_sector) * num_pages);
 	bank->base = 0x00000000;
-	num_pages = 2;				/* start with smallest Flash pages number */
 	bank->size = 32 * 1024;		/* bytes */
 
 	bank->sectors[0].offset = 0;

src/target/target.c
@@ -5058,7 +5058,6 @@ static int jim_target_smp(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
 	struct target_list *head, *curr, *new;
 	curr = (struct target_list *) NULL;
 	head = (struct target_list *) NULL;
-	new = (struct target_list *) NULL;
 
 	retval = 0;
 	LOG_DEBUG("%d", argc);

riscv-collab/riscv-openocd

commit 54cfcbca0cc7fe9e6667ea0559a9d5b104bcaf5a

[TO UPSTREAM] rtos/linux: fix name overwrite in `linux_thread_extra_info()`
commit 908ee4dc9641bd3df2eb00264575501867da539d (build: remove clang
unused variable assignment warnings) introduced an error:
```
-            tmp_str_ptr += sprintf(tmp_str_ptr, "%s", name);
+            sprintf(tmp_str_ptr, "%s", name);
             sprintf(tmp_str_ptr, "%s", temp->name);
```
This results in `name` being overwritten by `temp->name`.
Fix this, adding OOM handling along the way.

Change-Id: Id41f73247c3f7e6194d7c92187ad3163a9ea6c89
Signed-off-by: Evgeniy Naydanov <evgeniy.naydanov@syntacore.com>

src/rtos/linux.c
@@ -1119,23 +1119,26 @@ static int linux_thread_extra_info(struct target *target,
 
 	while (temp) {
 		if (temp->threadid == threadid) {
-			char *pid = " PID: ";
-			char *pid_current = "*PID: ";
-			char *name = "Name: ";
-			int str_size = strlen(pid) + strlen(name);
-			char *tmp_str = calloc(1, str_size + 50);
-			char *tmp_str_ptr = tmp_str;
-
-			/*  discriminate current task */
-			if (temp->status == 3)
-				tmp_str_ptr += sprintf(tmp_str_ptr, "%s",
-						pid_current);
-			else
-				tmp_str_ptr += sprintf(tmp_str_ptr, "%s", pid);
-
-			tmp_str_ptr += sprintf(tmp_str_ptr, "%d, ", (int)temp->pid);
-			sprintf(tmp_str_ptr, "%s", name);
-			sprintf(tmp_str_ptr, "%s", temp->name);
+			const char *pid = temp->status == 3
+				? "*PID: " /*  discriminate current task */
+				: " PID: ";
+			const char *name = "Name: ";
+			int str_size = strlen(pid) + strlen(name) + 50;
+			char *tmp_str = calloc(1, str_size);
+			if (!tmp_str) {
+				LOG_ERROR("Out of memory");
+				return ERROR_FAIL;
+			}
+			int res = snprintf(tmp_str, str_size, "%s%d, %s%s", pid,
+					(int)temp->pid, name, temp->name);
+			if (res < 0) {
+				LOG_ERROR("Failed to format the info: encoding error");
+				return ERROR_FAIL;
+			}
+			if (res >= str_size) {
+				LOG_ERROR("Failed to format the info: buffer is too small");
+				return ERROR_FAIL;
+			}
 			char *hex_str = calloc(1, strlen(tmp_str) * 2 + 1);
 			size_t pkt_len = hexify(hex_str, (const uint8_t *)tmp_str,
 				strlen(tmp_str), strlen(tmp_str) * 2 + 1);