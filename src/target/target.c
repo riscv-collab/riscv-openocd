ffed7bbbdba0a3afc5329cf10c4c0aa82486845b
Vishwajith26 <vishwajith@mindgrovetech.in>
2025-03-10 17:19:24 +0530

    Completed changing our sdk qspi driver in terms of openocd driver

--- src/target/target.c
+++ src/target/target.c
@@ -192,13 +192,13 @@ uint32_t QSPI_Transaction(struct target *target,uint32_t instance_number,qspi_ms
     uint32_t qspi_base = 0x40000+(instance_number*0x100);
     uint32_t cr_address = qspi_base+0x00;
     uint32_t dcr_address = qspi_base+0x04;
-    // uint32_t sr_address = qspi_base+0x08;
+    uint32_t sr_address = qspi_base+0x08;
     uint32_t fcr_address = qspi_base+0x0c;
     uint32_t dlr_address = qspi_base+0x10;
     uint32_t ccr_address = qspi_base+0x14;
     uint32_t ar_address = qspi_base+0x18;
     uint32_t abr_address = qspi_base+0x1c;
-    // uint32_t dr_address = qspi_base+0x20;
+    uint32_t dr_address = qspi_base+0x20;
 
     target_write_u32(target, cr_address,(CR_PRESCALER(msg->PRESCALER) |CR_PMM(msg->PMM) | CR_APMS(msg->APMS) | CR_TOIE(msg->TOIE) |CR_SMIE(msg->SMIE) | CR_FTIE(msg->FTIE) | CR_TCIE(msg->TCIE) | CR_TEIE(msg->TEIE) | CR_TCEN(msg->TOIE) | CR_EN(1)));         
     target_write_u32(target, dcr_address,(DCR_FSIZE(msg->FMEM_SIZE) | DCR_CKMODE(msg->CLK_MODE)));    
@@ -217,60 +217,78 @@ uint32_t QSPI_Transaction(struct target *target,uint32_t instance_number,qspi_ms
     target_write_u32(target, ar_address,msg->address);
     target_write_u32(target, abr_address,msg->alternate_byte);
 
-    // uint8_t i = 0;
-    // uint32_t status_reg;
-    // if(msg->functional_mode == CCR_FMODE_INDIRECT_WRITE && msg->length!= 0)
-    // {
+    uint8_t i = 0;
+    uint32_t status_reg;
+	uint32_t cr_value;
+    if(msg->functional_mode == CCR_FMODE_INDIRECT_WRITE && msg->length!= 0)
+    {
     //   QUADSPI_Reg(instance_number)->CR&= ~(CR_FTHRES(15));
-    //   while(1){
-    //     status_reg = QUADSPI_Reg(instance_number)->SR;
-    //     status_reg &= SR_FTF;
-    //     if(status_reg){
-    //       QUADSPI_Reg(instance_number)->DR.data_8 = msg->data_buffer[i];
-    //       i++;
-    //         if(i == msg->length)
-    //           break;
-    //     }
-    //   }
-    //   do{
-    //       temp = QUADSPI_Reg(instance_number)->SR;
-    //       temp&=SR_FLEVEL(FIFO_EMPTY);
-    //     }while(temp != 0);
+      target_read_u32(target,cr_address,&cr_value);
+	  cr_value&=~(CR_FTHRES(15));
+	  target_write_u32(target, cr_address,cr_value);
+	  while(1){
+		target_read_u32(target,sr_address,&status_reg);
+        //status_reg = QUADSPI_Reg(instance_number)->SR;
+        status_reg &= SR_FTF;
+        if(status_reg){
+			target_write_u8(target,dr_address,msg->data_buffer[i]);
+          i++;
+            if(i == msg->length)
+              break;
+        }
+      }
+      do{
+        //   temp = QUADSPI_Reg(instance_number)->SR;
+		target_read_u32(target,sr_address,&temp);
+          temp&=SR_FLEVEL(FIFO_EMPTY);
+        }while(temp != 0);
         
-    // }
-    // else if(msg->functional_mode == CCR_FMODE_INDIRECT_READ)
-    // {
+    }
+    else if(msg->functional_mode == CCR_FMODE_INDIRECT_READ)
+    {
     //   QUADSPI_Reg(instance_number)->CR&= ~(CR_FTHRES(15));
-    //   while(1){
-    //     status_reg = QUADSPI_Reg(instance_number)->SR;
-    //     status_reg &= SR_FTF;
-    //     if(status_reg){
-    //       msg->data_buffer[i] = QUADSPI_Reg(instance_number)->DR.data_8;
-    //       i++;
-    //         if(i == msg->length)
-    //           break;
-    //     }
-    //   }
-    // }
-    // if(msg->functional_mode == CCR_FMODE_MMM && msg->mm_mode == CCR_MM_MODE_RAM){
+	target_read_u32(target,cr_address,&cr_value);
+	cr_value&=~(CR_FTHRES(15));
+	target_write_u32(target, cr_address,cr_value);
+      while(1){
+        // status_reg = QUADSPI_Reg(instance_number)->SR;
+		target_read_u32(target,sr_address,&status_reg);
+        status_reg &= SR_FTF;
+        if(status_reg){
+			target_read_u8(target,dr_address,&msg->data_buffer[i]);
+        //   msg->data_buffer[i] = QUADSPI_Reg(instance_number)->DR.data_8;
+          i++;
+            if(i == msg->length)
+              break;
+        }
+      }
+    }
+    if(msg->functional_mode == CCR_FMODE_MMM && msg->mm_mode == CCR_MM_MODE_RAM){
     //   QUADSPI_Reg(instance_number)->CR&=~(CR_FTHRES(15));
-    //   QUADSPI_Reg(instance_number)->CR|=(CR_FTHRES(msg->fthresh));
-    //   return 0;
-    // }
-    // if(msg->functional_mode == CCR_FMODE_MMM && msg->mm_mode == CCR_MM_MODE_XIP){
+	target_read_u32(target,cr_address,&cr_value);
+	cr_value&=~(CR_FTHRES(15));
+	cr_value|=(CR_FTHRES(msg->fthresh));
+	target_write_u32(target, cr_address,cr_value);
+      return 0;
+    }
+    if(msg->functional_mode == CCR_FMODE_MMM && msg->mm_mode == CCR_MM_MODE_XIP){
     //   QUADSPI_Reg(instance_number)->CR&=~(CR_FTHRES(15));
-    //   return 0;
-    // }
+	target_read_u32(target,cr_address,&cr_value);
+	cr_value&=~(CR_FTHRES(15));
+	target_write_u32(target, cr_address,cr_value);
+      return 0;
+    }
 
-    // if(msg->data_mode == CCR_DMODE_NO_DATA)
-    // {
-    //   do{
+    if(msg->data_mode == CCR_DMODE_NO_DATA)
+    {
+      do{
     //   temp = QUADSPI_Reg(instance_number)->SR;
-    //   temp &= SR_TCF;
-    // }while(temp == 0);
-    // }
-    // GPT_Delay_Microsecs_H(100);   
-    // QUADSPI_Reg(instance_number)->CR&= ~CR_EN(1);
+	target_read_u32(target,sr_address,&temp);
+      temp &= SR_TCF;
+    }while(temp == 0);
+    }
+    usleep(100); 
+    target_write_u32(target, cr_address,0x00);
     return 0;
 }
 /***************************secure iot qspi stuff ends**********************************/
