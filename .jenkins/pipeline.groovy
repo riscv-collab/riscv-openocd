riscv-collab/riscv-openocd

commit c613f99aa86497a1ea79847719c17e9c934917ec

Use default Conan Autotools utils for building the project YCAT-42850
Previously we had CMake that wrapped OpenOCD as an external project.
This limits customizability of the build process and requires to
explicitly pass all the configuration through the CMake.

Imagine you want to modify how the project is build.
You will either:
1. Introduce an additional CMake argument and expand it in the external
   project's call to `configure`. Introduce MakePy argument to forward
   it to the CMake.
2. Avoid all that and just use `mpy -c sh ...` for configure and build.

I have found myself using the (2) option. Moreover, I've used it in CI
(`build-the-docs`, `tests-on-dummy`). I just couldn't forse myself to do
the extra work. TBH, I don't feel like this work is necessary. One less
layer just makes the system simpler, thats all.

So in this commit:
1. MakePy commands (extra ones from `.makepy/setup.py`) are no longer
   used to build OpenOCD. You can either:
   -  Run `mpy conan build`. This comand will run all the steps
      necessary to build the project replacing the sequence of
      `just-config` and `build`. There is a drawback though: you no
      longer have granular control. You cant just run the `build`.
      Also, `conan build` uses `--output-folder` instead of
      `--build-folder`. `--output-folder == --build-folder/../..`
2. If you want to run only `configure` or `build` you run them via
   `mpy -c sh ...` This may be not as convinient, but it's much more
   extendable.

To accomodate this changes CI was re-done.

Change-Id: I593d1d53f7072396b5d7a958456efaefe66ab926
Signed-off-by: Evgeniy Naydanov <evgeniy.naydanov@syntacore.com>

.jenkins/pipeline.groovy
@@ -2,13 +2,6 @@
 
 import tools.automation.TI
 
-def buildProject(vars, target = "openocd") {
-  sh(""" ./make.py just-config --profile:host ${vars.profile} \
-          --settings:host "&:build_type=${vars.buildType}" \
-          --build-path build/${vars.buildType} ${vars.extraOpts} """)
-  sh("./make.py build --build-path build/${vars.buildType} --target ${target}")
-}
-
 workflow('openocd') {
 
     parameters {
@@ -35,7 +28,11 @@ workflow('openocd') {
             fs('1.0Gi', '1.0Gi')
         }
         matrix {
-            [["image": ['cpp_ubuntu_20']]]
+            [
+                [
+                    "image": ['cpp_ubuntu_22']
+                ]
+            ]
         }
         shellScript {
             '''
@@ -51,17 +48,24 @@ workflow('openocd') {
             fs('1.0Gi', '1.2Gi')
         }
         matrix {
-            [[image          : ['cpp_centos_7', 'cpp_rocky_8', 'cpp_ubuntu_18', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
-              extraOpts      : ['', '--options:host elct_support=True'],
-              buildType      : ['Release'],
-              profile        : ['default']],
-             [image          : ['cpp_ubuntu_22'],
-              extraOpts      : [''],
-              buildType      : ['Release'],
-              extraOpts      : ['', '--options:host elct_support=True'],
-              profile        : ['makepy_sc_mingw', 'mp_armhf']]]
+            [
+                [
+                    image    : ['cpp_centos_7', 'cpp_rocky_8', 'cpp_ubuntu_18', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    profile  : ['default'],
+                    o_source : ['internal', 'syntacore']
+                ], [
+                    image    : ['cpp_ubuntu_22'],
+                    profile  : ['makepy_sc_mingw', 'mp_armhf'],
+                    o_source : ['internal', 'syntacore']
+                ]
+            ]
         }
-        script { vars -> buildProject(vars) }
+        shellScript {
+            '''
+                source .jenkins/common.sh
+                mpy conan create ${VARS_as_conan_args[@]}
+            '''
+        }
     }
 
     job('tests-sanitized') {
@@ -71,33 +75,75 @@ workflow('openocd') {
             fs('12.5Gi', '22.6Gi')
         }
         dependsOn 'lint' // workaround for better stage scheduling
+            matrix {
+                [
+                    [
+                        image        : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                        profile      : ['default'],
+                        target       : ['riscv_tests', 'OpenOCDTestsOn_spike'],
+                        o_source     : ['internal', 'syntacore'],
+                        s_build_type : ['Debug', 'Release'],
+                        // TODO: Enable 'strict' sanitize level YCAT-43092
+                        o_sanitize   : ['enable'],
+                    ]
+                ]
+            }
+        rules { vars ->
+            include(vars.ti >= TI.POSTCOMMIT)
+        }
+        shellScript {
+            '''
+                source .jenkins/common.sh
+                workdir=build/testsuite
+
+                runTests $workdir
+                EXIT_CODE=$?
+
+                sc-jenkins-lib artifacts push \
+                    "artifacts-${VARS_job}-${VARS_image}-${VARS_profile}-${VARS_target}-${VARS_o_source}-${VARS_s_build_type}-${VARS_o_sanitize}" \
+                    "${workdir}/testing" \
+                    1w
+                exit $EXIT_CODE
+            '''
+        }
+    }
+
+    job('tests-valgrind') {
+        resources {
+            cpu('10.0', '10')
+            memory('0.6Gi', '23.5Gi')
+            fs('13.0Gi', '30.1Gi')
+        }
+        dependsOn 'lint' // workaround for better stage scheduling
         matrix {
-             [[image          : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
-               profile        : ['default'],
-               testingType    : ['spike'],
-               // unfortunatly, we can't enable Strict sanitization, since jimtcl has bugs like this:
-               // https://github.com/msteveb/jimtcl/issues/300
-               // https://github.com/msteveb/jimtcl/issues/301
-               extraOpts      : ['--options:host test=True --sanitize-level=Enabled',
-                                 '--options:host test=True --sanitize-level=Enabled --options:host elct_support=True',
-                                 '--options:host test=True --tests-options tests-valgrid-path=valgrind',
-                                 '--options:host test=True --tests-options tests-valgrid-path=valgrind --options:host elct_support=True'],
-               buildType      : ['Debug', 'Release']]]
+            [
+                [
+                    image        : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    profile      : ['default'],
+                    target       : ['riscv_tests', 'OpenOCDTestsOn_spike'],
+                    o_source     : ['internal', 'syntacore'],
+                    s_build_type : ['Debug', 'Release'],
+                ]
+            ]
         }
         rules { vars ->
             include(vars.ti >= TI.POSTCOMMIT)
         }
-        script { vars ->
-            buildProject(vars)
-            try {
-              sh("./make.py build --build-path build/${vars.buildType} --target OpenOCDTestsOn_spike --parallel 8")
-              sh("./make.py sh ./.makepy/support/utils/check_sanitizer_logs.sh build/${vars.buildType}/testing/dejagnu")
-            } catch (Exception ex) {
-              artifacts.push("build/${vars.buildType}/testing",
-                             "artifacts-${vars.testingType}-${vars.image}-${vars.profile}-${vars.buildType}",
-                             retention: '1w')
-              error "wasted!"
-            }
+        shellScript {
+            '''
+                source .jenkins/common.sh
+                workdir=build/testsuite
+
+                VARS_tests_valgrind_path=$(which valgrind)
+                runTests $workdir
+                EXIT_CODE=$?
+
+                sc-jenkins-lib artifacts push \
+                    "artifacts-${VARS_job}-${VARS_image}-${VARS_profile}-${VARS_target}-${VARS_o_source}" \
+                    "${workdir}/testing" \
+                    1w
+                exit $EXIT_CODE
+            '''
         }
     }
 
@@ -109,27 +155,29 @@ workflow('openocd') {
         }
         dependsOn 'lint' // workaround for better stage scheduling
         matrix {
-            [[image          : ['cpp_rocky_8', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
-              profile        : ['default'],
-              testingType    : ['spike', 'external'],
-              extraOpts      : ['--options:host test=True',
-                                '--options:host elct_support=True --options:host test=True'],
-              buildType      : ['Release']]]
+            [
+                [
+                    image    : ['cpp_rocky_8', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    profile  : ['default'],
+                    target   : ['riscv_tests', 'OpenOCDTestsOn_spike'],
+                    o_source : ['internal', 'syntacore'],
+                ]
+            ]
         }
-        script { vars ->
-          buildProject(vars)
-          try {
-            if (vars.testingType == 'spike') {
-              sh("./make.py build --build-path build/${vars.buildType} --target OpenOCDTestsOn_spike --parallel 8")
-            } else {
-              sh("./make.py build --build-path build/${vars.buildType} --target RISCVTestsDebug --parallel 8")
-            }
-          } catch (Exception ex) {
-            artifacts.push("build/${vars.buildType}/testing",
-                           "artifacts-${vars.testingType}-${vars.image}-${vars.profile}-${vars.buildType}",
-                           retention: '1w')
-            error "wasted!"
-          }
+        shellScript {
+            '''
+                source .jenkins/common.sh
+                workdir=build/testsuite
+
+                runTests $workdir
+                EXIT_CODE=$?
+
+                sc-jenkins-lib artifacts push \
+                    "artifacts-${VARS_job}-${VARS_image}-${VARS_profile}-${VARS_target}-${VARS_o_source}" \
+                    "${workdir}/testing" \
+                    1w
+                exit $EXIT_CODE
+            '''
         }
     }
 
@@ -141,23 +189,29 @@ workflow('openocd') {
         }
         dependsOn 'lint' // workaround for better stage scheduling
         matrix {
-            [[image          : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
-              profile        : ['default'],
-              extraOpts      : ['--options:host test=True',
-                                '--options:host elct_support=True --options:host test=True'],
-              buildType      : ['Release']]]
+            [
+                [
+                    image    : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    profile  : ['default'],
+                    target   : ['riscv_tests', 'OpenOCDTestsOn_spike'],
+                    o_source : ['internal', 'syntacore'],
+                ]
+            ]
         }
-        script { vars ->
-            buildProject(vars)
-            try {
-                sh("./make.py build --build-path build/${vars.buildType} --target transferable_testsuite")
-                sh("./make.py build --build-path build/${vars.buildType} --target Transferable_OpenOCDTestsOn_spike --parallel 8")
-            } catch (Exception ex) {
-                artifacts.push("build/${vars.buildType}/testing",
-                    "artifacts-transferable--${vars.image}-${vars.profile}-${vars.buildType}",
-                    retention: '1w')
-                error "wasted!"
-            }
+        shellScript {
+            '''
+                source .jenkins/common.sh
+                workdir=build/testsuite
+
+                runTransferableTests $workdir
+                EXIT_CODE=$?
+
+                sc-jenkins-lib artifacts push \
+                    "artifacts-${VARS_job}-${VARS_image}-${VARS_profile}-${VARS_target}-${VARS_o_source}" \
+                    "${workdir}/testing" \
+                    1w
+                exit $EXIT_CODE
+            '''
         }
     }
 
@@ -168,20 +222,32 @@ workflow('openocd') {
             fs('5.8Gi', '13.5Gi')
         }
         matrix {
-            [[image          : ['cpp_ubuntu_20'],
-              profile        : ['default'],
-              extraOpts      : ['--options:host elct_support=True --options:host test=True'],
-              buildType      : ['Release']]]
+            [
+                [
+                    image    : ['cpp_ubuntu_22'],
+                    profile  : ['default'],
+                    o_source : ['internal'],
+                ]
+            ]
         }
         rules { vars ->
             include(vars.ti >= TI.POSTCOMMIT)
         }
-        script { vars ->
-            buildProject(vars, "transferable_testsuite")
-            artifacts.push("build/${vars.buildType}/install_testsuite", "${vars.profile}/${vars.image}/testsuite'")
+        shellScript {
+            '''
+                source .jenkins/common.sh
+                workdir=build/testsuite
 
-            sh("./make.py sh make fpga_configuration_registry -f testing/syntacore/fpga_support/makefile")
-            artifacts.push("fpga_info", "${vars.profile}/${vars.image}/fpga_info'")
+                buildTransferableTestsuite $workdir
+                sc-jenkins-lib artifacts push \
+                    "${VARS_profile}/${VARS_image}/testsuite" \
+                    "${workdir}/install_testsuite"
+
+                mpy sh make fpga_configuration_registry -f testing/syntacore/fpga_support/makefile
+                sc-jenkins-lib artifacts push \
+                    "${VARS_profile}/${VARS_image}/testsuite" \
+                    fpga_info
+            '''
         }
     }
 
@@ -194,10 +260,13 @@ workflow('openocd') {
         }
         dependsOn 'tests-fpga-build'
         matrix {
-            [[image          : ['cpp_ubuntu_20'],
-              profile        : ['default'],
-              buildType      : ['Release'],
-              stand          : ['twin']]]
+            [
+                [
+                    image   : ['cpp_ubuntu_22'],
+                    profile : ['default'],
+                    stand   : ['twin']
+                ]
+            ]
         }
         rules { vars ->
             include(vars.ti >= TI.POSTCOMMIT)
@@ -261,8 +330,12 @@ workflow('openocd') {
             fs('1.0Gi', '1.0Gi')
         }
         matrix {
-            [[image          : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
-              env            : ['', 'CC=clang CFLAGS=-fsanitize=address,undefined LDFLAGS=-Wl,-ldl']]]
+            [
+                [
+                    image : ['cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    env   : ['', 'CC=clang CFLAGS=-fsanitize=address,undefined LDFLAGS=-Wl,-ldl'],
+                ]
+            ]
         }
         shellScript {
             '''
@@ -283,7 +356,11 @@ workflow('openocd') {
             fs('1.0Gi', '1.0Gi')
         }
         matrix {
-            [[image          : ['cpp_ubuntu_22']]]
+            [
+                [
+                    image          : ['cpp_ubuntu_22'],
+                ]
+            ]
         }
         shellScript {
             '''
@@ -304,15 +381,17 @@ workflow('openocd') {
         }
         vars { [name: "openocd"] }
         matrix {
-            [[image          : ['cpp_centos_7', 'cpp_rocky_8', 'cpp_ubuntu_18', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
-              profile        : ['default'],
-              extraArgs      : ['', '--options:host elct_support=True']],
-             [image          : ['cpp_ubuntu_22'],
-              profile        : ['makepy_sc_mingw'],
-              extraArgs      : ['', '--options:host elct_support=True']],
-             [image          : ['cpp_ubuntu_22'],
-              profile        : ['mp_armhf'],
-              extraArgs      : ['--options:host elct_support=True']]]
+            [
+                [
+                    image    : ['cpp_centos_7', 'cpp_rocky_8', 'cpp_ubuntu_18', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    profile  : ['default'],
+                    o_source : ['internal', 'syntacore']
+                ], [
+                    image    : ['cpp_ubuntu_22'],
+                    profile  : ['makepy_sc_mingw', 'mp_armhf'],
+                    o_source : ['internal', 'syntacore']
+                ]
+            ]
         }
     }
 

.ci/openocd_tests_hw.Jenkinsfile
@@ -1,3 +1,4 @@
+// Needs update to the new interface
 def boards = []
 def UploadResults = 0
 def workspaceId

.gitignore
@@ -122,7 +122,6 @@ fpga_info/
 
 conaninfo.txt
 conanmanifest.txt
-__sc_version.txt
 .tests.stand.info.json
 
 *.pyc

.jenkins/common.sh
@@ -1,0 +1,69 @@
+set -e
+set -u
+
+declare -a VARS_as_conan_args
+VARS_as_conan_args=(
+  -pr ${VARS_profile:=default}
+  -s openocd/*:build_type=${VARS_s_build_type:=Release}
+  -o openocd/*:source=${VARS_o_source:=internal}
+  -o openocd/*:sanitize=${VARS_o_sanitize:=disable}
+)
+
+installOpenOCDto() {
+  ocd_install_path=$1
+  mpy conan build ${VARS_as_conan_args[@]}
+  mkdir -p $ocd_install_path
+  mpy sh make \
+    -C build/$VARS_s_build_type \
+    install -j \
+    DESTDIR=$(realpath $ocd_install_path)
+}
+
+configureTestsuite() {
+  workdir=$1
+  ocd_install="${workdir}/openocd_install"
+
+  installOpenOCDto $ocd_install
+
+  mpy conan install ${VARS_as_conan_args[@]} \
+    --name openocd_testsuite \
+    --output-folder $workdir
+
+  #TODO make openocd-install absolute in makepy
+  mpy config --build-path ${workdir} \
+    --openocd-install $(realpath $ocd_install)
+}
+
+testsBuild() {
+  workdir=$1
+
+  mpy build --build-path ${workdir} \
+    ${VARS_tests_adapter_info:+--tests-adapter-info ${VARS_tests_adapter_info}} \
+    ${VARS_tests_valgrind_path:+--tests-valgrind-path ${VARS_tests_valgrind_path}} \
+    --target=$VARS_target
+}
+
+runTests() {
+  workdir=$1
+
+  configureTestsuite $workdir
+
+  testsBuild $workdir
+}
+
+buildTransferableTestsuite() {
+  workdir=$1
+
+  configureTestsuite $workdir
+
+  mpy build --build-path ${workdir} \
+    --target transferable_testsuite
+}
+
+runTransferableTests() {
+  workdir=$1
+
+  buildTransferableTestsuite $workdir
+
+  testsBuild $workdir
+}

.makepy/setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-import itertools
 import logging
-import shutil
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
@@ -19,175 +17,6 @@ from makepy.utils import run_shell
 
 _logger = logging.getLogger()
 _repo_path = Path(__file__).parent.parent
-
-
-class _JustConfigCommand(Command):
-    def name(self) -> str:
-        return "just-config"
-
-    def help(self) -> str:
-        return "Just config the project!"
-
-    def amend_parser(self, parser: ArgumentParser) -> None:
-        conan_help = "See './make.py conan install --help'."
-        parser.add_argument(
-            "--build", type=str, default="never", help=conan_help
-        )
-        parser.add_argument(
-            "-b",
-            "--build-path",
-            required=True,
-            type=Path,
-            help="Build directory.",
-        )
-        parser.add_argument(
-            "-pr",
-            "--profile",
-            "-pr:h",
-            "--profile:host",
-            dest="host_profile",
-            type=str,
-            default="default",
-            help=conan_help,
-        )
-        parser.add_argument(
-            "-pr:b",
-            "--profile:build",
-            dest="build_profile",
-            type=str,
-            default="default",
-            help=conan_help,
-        )
-        parser.add_argument(
-            "-o",
-            "--options",
-            "-o:h",
-            "--options:host",
-            dest="host_options",
-            type=str,
-            default=[],
-            action="append",
-            help=conan_help,
-        )
-        parser.add_argument(
-            "-o:b",
-            "--options:build",
-            dest="build_options",
-            type=str,
-            default=[],
-            action="append",
-            help=conan_help,
-        )
-        parser.add_argument(
-            "-s",
-            "--settings",
-            "-s:h",
-            "--settings:host",
-            dest="host_settings",
-            type=str,
-            default=[],
-            action="append",
-            help=conan_help,
-        )
-        parser.add_argument(
-            "-s:b",
-            "--settings:build",
-            dest="build_settings",
-            type=str,
-            default=[],
-            action="append",
-            help=conan_help,
-        )
-
-        parser.add_argument(
-            "--tests-options",
-            dest="tests_options",
-            type=str,
-            default=[],
-            action="append",
-            help="additional parameters relevant for testing",
-        )
-        parser.add_argument(
-            "--sanitize-level",
-            dest="sanitize_level",
-            type=str,
-            default=None,
-            help="ASan/UBSan sanitizaion flags",
-        )
-
-    def _get_output_folder(self, build_path: Path) -> Path:
-        msg = (
-            "Conan will always generate files into build/(Debug|Release), "
-            "so your build path should be in the format <any_mangle>/build/(Debug|Release). "
-            "You can change this behavior in conanfile.py."
-        )
-        if build_path.name not in ["Debug", "Release"]:
-            raise ValueError(msg)
-        if build_path.parent.name != "build":
-            raise ValueError(msg)
-        return build_path.parent.parent
-
-    def command(self, args: Namespace) -> None:
-        shutil.rmtree(_repo_path / "external_sources", ignore_errors=True)
-        makepy = [
-            _repo_path / "make.py",
-            "--no-history-dump",
-            "--logging-level",
-            args.logging_level.lower(),
-        ]
-        output_folder = self._get_output_folder(args.build_path)
-        install_cmd = [
-            *makepy,
-            "conan",
-            "install",
-            "--profile:host",
-            args.host_profile,
-            "--profile:build",
-            args.build_profile,
-            "--build",
-            args.build,
-            "--output-folder",
-            output_folder,
-        ]
-        install_cmd.extend(
-            itertools.chain.from_iterable(
-                ["--settings:host", setting] for setting in args.host_settings
-            )
-        )
-        install_cmd.extend(
-            itertools.chain.from_iterable(
-                ["--settings:build", setting] for setting in args.build_settings
-            )
-        )
-        install_cmd.extend(
-            itertools.chain.from_iterable(
-                ["--options:host", setting] for setting in args.host_options
-            )
-        )
-        install_cmd.extend(
-            itertools.chain.from_iterable(
-                ["--options:build", setting] for setting in args.build_options
-            )
-        )
-
-        # TODO: should we remove all these "cwd" statements?
-        run_shell(install_cmd, cwd=_repo_path)
-        config_cmd = [
-            *makepy,
-            "config",
-            "--build-path",
-            args.build_path,
-        ]
-
-        config_cmd.extend(
-            itertools.chain.from_iterable(
-                [f"--{option}"] for option in args.tests_options
-            )
-        )
-        if args.sanitize_level is not None:
-            config_cmd.extend([f"--sanitize-level={args.sanitize_level}"])
-
-        run_shell(config_cmd, cwd=_repo_path)
 
 
 class _ConfigCommand(Command):
@@ -213,17 +42,14 @@ class _ConfigCommand(Command):
             help="run OpenOCD under valgrid when running tests",
         )
         parser.add_argument(
-            "--sanitize-level",
-            dest="sanitize_level",
-            choices=[None, "Enabled", "Strict"],
+            "--openocd-install",
+            dest="openocd_install",
             type=str,
-            default=None,
-            help="controls ASan/UBSan behavior if enabled",
+            required=True,
+            help="path to OpenOCD install",
         )
 
     def command(self, args: Namespace) -> None:
-        shutil.rmtree(_repo_path / "build-aux", ignore_errors=True)
-        run_shell(["./bootstrap", "nosubmodule"], cwd=_repo_path)
         cmd = [
             "cmake",
             "-S",
@@ -246,17 +72,16 @@ class _ConfigCommand(Command):
                 [f"-DOPENOCD_TESTS_VALGRIND_PATH={args.tests_valgrind_path}"]
             )
 
-        if args.sanitize_level is None:
-            pass
-        elif args.sanitize_level == "Enabled":
-            cmd.extend(["-DSC_OPENOCD_ENABLE_SANITIZERS=ON"])
-        elif args.sanitize_level == "Strict":
-            cmd.extend(["-DSC_OPENOCD_ENABLE_SANITIZERS=ON"])
-            cmd.extend(["-DSC_OPENOCD_STRICT_SANITIZERS=ON"])
-        else:
-            raise ValueError(
-                f"unknown sanitization level {args.sanitize_level}"
-            )
+        cmd.extend(
+            [
+                f"-DRISCVSpike_DIR={args.hints.host['riscv-isa-sim'].vars['SC_SPIKE_PATH']}",
+                f"-DRISCVGCC_DIR={args.hints.host['riscv-gcc'].vars['SC_GCC_PATH']}",
+                f"-DRISCVGDB_DIR={args.hints.host['riscv-gdb'].vars['SC_RISCV_GDB_PATH']}",
+                f"-DDEJAGNU_DIR={args.hints.host['dejagnu'].vars['SC_DEJAGNU_PATH']}",
+                f"-DRISCVTESTS_DIR={args.hints.host['external_openocd_tests'].vars['SC_EXTERNAL_OPENOCD_TESTS_PATH']}",
+                f"-DOPENOCD_INSTALL_PATH={args.openocd_install}",
+            ]
+        )
         run_shell(cmd)
 
 
@@ -273,7 +98,7 @@ class _BuildCommand(Command):
             "--target",
             dest="cmake_target",
             type=str,
-            default="openocd",
+            required=True,
             help="Build target for CMake.",
         )
 
@@ -387,18 +212,30 @@ def main() -> None:
     conductor.add(GenericSuite())
     conductor.add(SyntacoreSuite())
     configs = ConanConfigs()
-    configs.add(profile="mp_armhf", options={"elct_support": True})
-    configs.add_windows(options={"elct_support": [False, True]})
-    configs.add_ubuntu18(options={"elct_support": [False, True]})
-    configs.add_centos7(options={"elct_support": [False, True]})
-    configs.add_ubuntu20(
-        options={"elct_support": [False, True], "test": [False, True]}
+    configs.add(
+        profile="mp_armhf", options={"source": ["internal", "syntacore"]}
     )
-    configs.add_ubuntu22(
-        options={"elct_support": [False, True], "test": [False, True]}
+    configs.add_windows(options={"source": ["internal", "syntacore"]})
+    configs.add_ubuntu18(options={"source": ["internal", "syntacore"]})
+    configs.add_centos7(options={"source": ["internal", "syntacore"]})
+    configs.add_ubuntu20(options={"source": ["internal", "syntacore"]})
+    configs.add_ubuntu22(options={"source": ["internal", "syntacore"]})
+    configs.add_rocky8(options={"source": ["internal", "syntacore"]})
+
+    conan = ConanSuite(
+        name="openocd",
+        start_version="2d580e9457771c9fe6bfd7b241a73ab65270d44e",
+        start_semver="0.12.2",
+        configs=configs,
+        release_branch="sc/stable",
+        recipe=_repo_path / "conanfile.py",
     )
-    configs.add_rocky8(
-        options={"elct_support": [False, True], "test": [False, True]}
+    conan.add_package(
+        name="openocd_testsuite",
+        start_version="e5b87c3864349cb6e9ab0df471c628ea93ae7d86",
+        start_semver="0.0.0",
+        recipe=_repo_path / "testsuite" / "conanfile.py",
+        configs=ConanConfigs().add_ubuntu20(),
     )
 
     conan = ConanSuite(
@@ -415,7 +252,6 @@ def main() -> None:
         configs=ConanConfigs().add_ubuntu22(),
     )
     conductor.add(conan)
-    conductor.add(_JustConfigCommand())
     conductor.add(_ConfigCommand())
     conductor.add(_BuildCommand())
 

commit 49818e6b06f137a858455235b542aed8f81849fb

[ci] fix deploy configurations YCAT-43364
Deploy got broken in commit c613f99aa86497a1ea79847719c17e9c934917ec ("
Use default Conan Autotools utils for building the project YCAT-42850").

The issue is: `deploy()` is not aware and does not use `o_source`.

Change-Id: I32701654f5aa269defb2fcea5139b1204b2748a8

.jenkins/pipeline.groovy
@@ -274,13 +274,13 @@ workflow('openocd') {
         matrix {
             [
                 [
-                    image    : ['cpp_centos_7', 'cpp_rocky_8', 'cpp_ubuntu_18', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
-                    profile  : ['default'],
-                    o_source : ['internal', 'syntacore']
+                    image     : ['cpp_centos_7', 'cpp_rocky_8', 'cpp_ubuntu_18', 'cpp_ubuntu_20', 'cpp_ubuntu_22'],
+                    profile   : ['default'],
+                    extraArgs : ['--options:host elct_support=True', '--options:host elct_support=False'],
                 ], [
-                    image    : ['cpp_ubuntu_22'],
-                    profile  : ['makepy_sc_mingw', 'mp_armhf'],
-                    o_source : ['internal', 'syntacore']
+                    image     : ['cpp_ubuntu_22'],
+                    profile   : ['makepy_sc_mingw', 'mp_armhf'],
+                    extraArgs : ['--options:host elct_support=True'],
                 ]
             ]
         }